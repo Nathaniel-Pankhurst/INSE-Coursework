10a1feb35caa6706fc4df8bffd6dba3f83ceef7b
Thomas Fox <thomasfox@Toms-MacBook-Pro-2.local>
2016-02-08 12:37:14 +0000

    tom

---
+++ js/google.js
@@ -0,0 +1,20 @@
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
+  gapi.load('auth2', function() {
+    auth2 = gapi.auth2.init({
+      client_id: 'CLIENT_ID.apps.googleusercontent.com',
+      fetch_basic_profile: false,
+      scope: 'profile'
+    });
+
+    // Sign the user in, and then retrieve their ID.
+    auth2.signIn().then(function() {
+      console.log(auth2.currentUser.get().getId());
+    });
+  });
