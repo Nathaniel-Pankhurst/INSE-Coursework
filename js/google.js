e7246f421924658c43034e19eb20a3de4c697716
Thomas Fox <thomasfox@eri-135-40.d.port.ac.uk>
2016-02-19 11:53:21 +0000

    Google sign works proper - redirects to chart page

--- js/google.js
+++ js/google.js
@@ -1,15 +1,3 @@
-
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
-
   function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
+    window.location.assign("http://localhost/INSE-Coursework/WBT.html");
   }
