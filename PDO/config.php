cf5833a6da5065bdaac839865a1bcc61ceab8707
Raife-White <raifew@hotmail.co.uk>
2016-02-22 12:28:23 +0000

    Update config.php

--- PDO/config.php
+++ PDO/config.php
@@ -1,47 +1,40 @@
 <?php
 /**
 * @author: UP745065
+* @author: UP737044
 */
 
 /*Databse Information*/
 const DBHOST = "localhost";     // Host Name
 const DBUSER= "root";           // Username for the databse
-const DBPASS = "";              // Database Password
-const DBNAME = "audioSphere";   // Database Name
+const DBPASS = "root";              // Database Password
+const DBNAME = "insewincy";   // Database Name
 
 /*Database Tables*/
 
-const DBTABLES =
-
-"CREATE TABLE Account(
-id INT PRIMARY KEY AUTO_INCREMENT,
-username VARCHAR(50) NOT NULL,
-password VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE Genre(
-id INT PRIMARY KEY AUTO_INCREMENT,
-genre_name VARCHAR(50) NOT NULL,
-genre_description TEXT NOT NULL
-);
-
-CREATE TABLE Post(
-id INT PRIMARY KEY AUTO_INCREMENT,
-userID INT NOT NULL,
-genreID INT NOT NULL,
-postInfomation TEXT NOT NULL,
-timeCreated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-CONSTRAINT userIDFK FOREIGN KEY (userID) REFERENCES Account(id),
-CONSTRAINT genreIDFK FOREIGN KEY (genreID) REFERENCES Genre(id)
-);
-
-CREATE TABLE Comment(
-id INT PRIMARY KEY AUTO_INCREMENT,
-userID INT NOT NULL,
-postID INT NOT NULL,
-commentInformation TEXT NOT NULL,
-timeCreated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-CONSTRAINT userIDFK FOREIGN KEY (userID) REFERENCES Account(id),
-CONSTRAINT postIDFK FOREIGN KEY (postID) REFERENCES Post(id)
-);"
+const DBTABLES = "CREATE TABLE Owner(
+  ID INT PRIMARY KEY,
+  Name VARCHAR(45) NOT NULL,
+  Email VARCHAR(45) NOT NULL
+)";
+  "CREATE TABLE Project (
+  ID INT PRIMARY KEY,
+  Name VARCHAR(45),
+  Owner-ID INT,
+  FOREIGN KEY (Owner-ID) REFERENCES Owner(ID)
+)";
+  "CREATE TABLE Object (
+  ID INT PRIMARY KEY,
+  Name VARCHAR(100),
+  Slack TIME,
+  Duration TIME,
+  Earliest_Start DATE,
+  Earliest_End DATE,
+  Latest_Start DATE,
+  Latest_End DATE,
+  Parent_object INT,
+  Project_id INT,
+  FOREIGN KEY (Project_id) REFERENCES Project(ID),
+  FOREIGN KEY (Parent_object) REFERENCES Object(ID)
+)";
 ?>
